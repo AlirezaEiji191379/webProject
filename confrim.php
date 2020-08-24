64a8d39df649b7d6fb64dd6c019070ec3dafa087
Alireza <alirezaeiji151379@gmail.com>
2020-08-24 00:06:57 -0700

    تقریبا زیبایی صفحه تایید ایمیل نیز انجام شد!

--- confrim.php
+++ confrim.php
@@ -19,18 +19,19 @@
     </head>
     <body>
     <div id="formDiv"dir="rtl">
-        <form id="myForm">
+        <p id="warning">
+            کد تاییدی برای پست الکترونیکی شما ارسال شده است!
+        </p>
 
+        <form id="myForm">
             <div id="div1">
-            <input type="text" name="code" id="code"  placeholder="کد تایید را وارد کنید"><br><br>
+            <input type="text" name="code" id="code"  placeholder="کد تایید را وارد کنید">
             </div>
+            <p id="clock"></p><br><br>
             <input type="submit" name="submit"  value="ارسال">
-            <div>
-                <p id="clock"></p>
-            </div>
+            <p><a href="" id="resendCode">کد تایید را دریافت نکردید؟</a> </p>
         </form>
     </div>
-
     </body>
 
 </html>
