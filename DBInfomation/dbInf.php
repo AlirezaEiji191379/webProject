e4de5775a3688b14747d7ddbba7d1f56c9d29fed
Alireza <alirezaeiji151379@gmail.com>
2020-09-11 13:19:33 -0700

    بهبود در ریجکس ها!

--- DBInfomation/dbInf.php
+++
@@ -1,5 +0,0 @@
-<?php
-$mysql_Username="alirezaeiji151379";
-$mysql_Password="alirezaeiji";
-$mysql_DBname="webproject";
-$mysql_Host="localHost";
--- Register/checkFromDB.php
+++ Register/checkFromDB.php
@@ -1,5 +1,17 @@
 <?php
+include_once("../DBInformation/dbInf.php");
 
+if(isset($_GET["field"])){
+    $mysql=new mysqli(host,username,password,dbname);
+
+
+    if($_GET["field"]==="username"){
+
+    }
+    elseif ($_GET["field"]==="email"){
+        echo "hello email";
+    }
+}
 
 
 
