eaf2aa5e01248a419947c1f0ef417cbee475180e
Alireza <alirezaeiji151379@gmail.com>
2020-09-12 12:01:01 -0700

    شماره تلفن رو یادم رفته بود!

--- loginPanel/addToDB.php
+++ loginPanel/addToDB.php
@@ -4,6 +4,15 @@ if(session_status()==PHP_SESSION_NONE) {
     session_start();
 }
 
+
+function makeSafe($input){
+    return stripcslashes(htmlspecialchars(trim($input)));
+}
+
+$username=makeSafe($_SESSION["username"]);
+
+
+
 if(isset($_COOKIE[session_name()])){
     setcookie(session_name(),"",time()-3600,"/");
     session_destroy();
